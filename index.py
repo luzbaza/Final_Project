2d6fd6cf2243a75746bb30df1412a14b768f8e82
sergiovelasquez18 <2458@holbertonschool.com>
2021-10-28 16:46:57 -0500

    update of files

--- index.py
+++ index.py
@@ -18,5 +18,10 @@ def connections():
 def contac():
 	return render_template('contac.html')
 
+@app.route('/pets')
+def pets():
+	return render_template('pets.html')
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port='5000', debug=True)
